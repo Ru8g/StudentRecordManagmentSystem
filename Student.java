28ae240c9d10cdd9c3c70610c8aab2f49d5ab3fd
Reema <famaary21@gmail.com>
2026-05-21 17:19:23 +0300

    spelling error change+creating cons

--- Student.java
+++ Student.java
@@ -2,8 +2,15 @@ public class Student {
 
     private String name;
     private int id;
-    private String deopartment;
+    private String department;
     private double gpa;
     private int year;
 
+public Student(int id,String name,String department,double gpa,int year){
+    this.name=name;
+    this.id=id;
+    this.department=department;
+    this.gpa=gpa;
+    this.year=year;  
+}
 }
